d027b17b0a4f3624235d8ee1902aa5fe6ccfb4e1
MikeHill89 <mvreugdenhil18@gmail.com>
2023-04-14 00:49:31 +0300

    Fix: grid size prompt, grid size lower than 16 or higher than 100

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,27 @@
 const drawingCanvas = document.querySelector(".container");
 const userColor = '#00ffff';
 const resetButton = document.getElementById('reset');
-const gridSize = 100;
-const canvasWidth = drawingCanvas.offsetWidth;
-const squareSize = canvasWidth / gridSize - 2;
-
+const gridButton = document.getElementById('gridsize');
 const getRandomColor = () => {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   return `#${randomColor}`;
 }
-function drawGrid() {
+const getGridSize = () => {
+  return parseInt(prompt("what grid size do you want"));
+};
 
-  for (let index = 0; index < gridSize * gridSize; index++) {
+function drawGrid() {
+  drawingCanvas.replaceChildren();
+  let setGridSize = getGridSize();
+  if (setGridSize > 100 || setGridSize < 16) {
+    setGridSize = 0;
+    alert("Sorry, you need to have a positive grid of atleast 16 to a maximum of a 100")
+    drawGrid();
+  }
+  const canvasWidth = drawingCanvas.offsetWidth;
+  const squareSize = (canvasWidth / setGridSize) - 2;
+
+  for (let index = 0; index < setGridSize * setGridSize; index++) {
     const gridBlock = document.createElement("div");
     gridBlock.classList.add("square");
     gridBlock.style.width = `${squareSize}px`;
@@ -22,21 +32,20 @@ function drawGrid() {
     gridBlock.style.backgroundColor = '#ffffff';
     drawingCanvas.appendChild(gridBlock);
   }
-
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.addEventListener('mouseover', colorSquare));
 }
-drawGrid();
 
-const squares = document.querySelectorAll('.square');
 function colorSquare() {
   this.style.backgroundColor = getRandomColor();
 }
 
 function resetGrid() {
+  const squares = document.querySelectorAll('.square');
   squares.forEach(square => square.style.backgroundColor = "");
 }
 
-squares.forEach(square => square.addEventListener('mouseover', colorSquare));
 
 resetButton.addEventListener('click', resetGrid);
-
-
+gridButton.addEventListener('click', drawGrid);
+drawGrid();
